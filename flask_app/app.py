c1a9aa309c1a40b438f32d0d20dfc75c89749edf
vaishakhanil <vaishakh.anil.360@gmail.com>
2024-10-12 20:29:43 -0400

    Step 3 & Step 4: Connecting to MongoDB Atlas and Creating Routes

---
+++ flask_app/app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# Setting up flask
+app = Flask(__name__)
+
+# Loading dotenv
+load_dotenv()
+
+# Getting the username and password from .env file
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+
+# Connecting to Mongo Atlas
+URI = "mongodb+srv://{}:{}@assignment2cluster.5lvu5.mongodb.net/?retryWrites=true&w=majority&appName=assignment2Cluster".format(MONGO_USERNAME, MONGO_PASSWORD)
+
+# Print Error Message
+if MONGO_PASSWORD == None || MONGO_USERNAME == None:
+    print("Connection Error: Please create .env file and enter your MongoDB Username and password")
+
+client = MongoClient(URI)
+db = client.shop_db
+products_collection = db.products
+
+
+# Routing to / to render home.html
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+# Routing to /products to render Products and display the products from the mongodb shop_db database
+@app.route("/products")
+def products():
+    products = products_collection.find()
+    return render_template("products.html", products)
+
+if __name__ == '__main__':
+    app.run()
